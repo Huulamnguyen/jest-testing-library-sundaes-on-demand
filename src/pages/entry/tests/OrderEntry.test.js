bf58fe52cd26d340cc07b23ab15a5425c0961455
huulamnguyen <liamnguyen.swe@gmail.com>
2022-04-12 16:15:25 -0400

    Test and code alert for server error response

--- src/pages/entry/tests/OrderEntry.test.js
+++ src/pages/entry/tests/OrderEntry.test.js
@@ -3,15 +3,15 @@ import OrderEntry from "../OrderEntry";
 import { rest } from "msw";
 import { server } from "../../../mocks/server";
 
-test.only("handles errors for scoops and toppings routes", async () => {
+test("handles errors for scoops and toppings routes", async () => {
   // Reset handlers
   server.resetHandlers(
-    rest.get("http://localhost:3030/scoops", (req, res, ctx) =>
-      res(ctx.status(500))
-    ),
-    rest.get("http://localhost:3030/toppings", (req, res, ctx) =>
-      res(ctx.status(500))
-    )
+    rest.get("http://localhost:3030/scoops", (req, res, ctx) => {
+      return res(ctx.status(500));
+    }),
+    rest.get("http://localhost:3030/toppings", (req, res, ctx) => {
+      return res(ctx.status(500));
+    })
   );
 
   render(<OrderEntry />);
